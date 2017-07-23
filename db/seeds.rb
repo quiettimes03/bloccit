f1fcb3e7cffd19d03d2a30af1c4b9bf95f2d95c6
Renee Bain <rbain02@gmail.com>
2017-07-23 11:58:53 -0400

    assignment20: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,17 @@ posts = Post.all
   )
 end
 
+#Create Questions
+10.times do
+  Question.create!(
+    title: RandomData.random_sentence,
+    body:  RandomData.random_paragraph,
+    resolved: false
+  )
+end
+question = Question.all
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"
