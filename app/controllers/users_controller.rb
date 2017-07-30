1a7881449fac1b36855e4b09f899b6e38efd9785
Renee Bain <rbain02@gmail.com>
2017-07-29 21:11:36 -0400

    assignment24: completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,14 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def confirm
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+  end
+
   def create
     @user = User.new
     @user.name = params[:user][:name]
@@ -11,6 +19,7 @@ class UsersController < ApplicationController
     @user.password_confirmation = params[:user][:password_confirmation]
 
     if @user.save
+      @user.confirm
       flash[:notice] = "Welcome to Bloccit #{@user.name}!"
       redirect_to root_path
     else
