933eecffaff04a82f3878ca9987d4849550f83d5
Renee Bain <rbain02@gmail.com>
2017-07-29 22:51:20 -0400

    checkpoint25: revised

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -55,7 +55,7 @@ RSpec.describe UsersController, type: :controller do
 
     it "logs the user in after sign up" do
       post :create, user: new_user_attributes
-      expect(session[:user_id]).to eq assigns (:user).id
+      expect(session[:user_id]).to eq assigns(:user).id
     end
   end
 end
